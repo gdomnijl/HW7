b1db8cba071492bb86dd702dceabbe747a5c4f71
gdomnijl <hejinlin@grinnell.edu>
2016-11-17 17:25:11 -0600

    Update Armor.java

--- Armor.java
+++ Armor.java
@@ -1,3 +1,7 @@
+/**
+	 * 
+	 * The class Armor holds two fields that store the minimun and maximum defense value of the armor as strings
+	 */
 public class Armor {
 	public String minVal; 
 	public String maxVal; 
