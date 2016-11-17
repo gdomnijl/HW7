42b06d3d931bad7927135778f704d04024a22ea3
gdomnijl <hejinlin@grinnell.edu>
2016-11-17 17:35:41 -0600

    Update MagicPrefix.java

--- MagicPrefix.java
+++ MagicPrefix.java
@@ -1,3 +1,10 @@
+/** 
+	 * The class MagicPrefix holds four sub-fields:
+	 * prefix: a string of prefix
+	 * power: store the type of the power as a string 
+	 * minVal: store the lower bound of the magic value as a string 
+	 * maxVal: store the upper bound of the magic value as a string 
+	 */
 public class MagicPrefix {
 	public String prefix; 
 	public String power; 
